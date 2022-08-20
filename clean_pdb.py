5db3bde34c106e8e8e2e7de6890d46f963943d46
wangxinglong1990 <71169505+wangxinglong1990@users.noreply.github.com>
2022-08-20 20:11:21 +0800

    Update clean_pdb.py

--- clean_pdb.py
+++ clean_pdb.py
@@ -1,14 +1,35 @@
 '''
+MIT License
+
+Copyright (c) 2021 Deva Lab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 Takes a PDB file and removes hetero atoms from its structure.
 First argument is path to original file, second argument is path to generated file
 '''
+
 from Bio.PDB import PDBParser, PDBIO, Select
 import Bio
 import os
 import sys
 import pybel
-
-database_dir = 'scPDB_classified/'
 
 
 class NonHetSelect(Select):
@@ -22,18 +43,4 @@ def clean_pdb(input_file, output_file):
     io = PDBIO()
     io.set_structure(pdb)
     io.save(output_file, NonHetSelect())
-
-
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        count = 0
-        for classification in os.listdir(database_dir):
-            class_dir = database_dir + classification + '/'
-            for name in os.listdir(class_dir):
-                count += 1
-                print(count, class_dir + name)
-                protein = next(pybel.readfile('mol2', class_dir + name + '/' + 'protein.mol2'))
-                protein.write('pdb', class_dir + name + '/' + 'protein.pdb')
-                clean_pdb(class_dir + name + '/' + 'protein.pdb', class_dir + name + '/' + 'protein.pdb')
-                os.remove(class_dir + name + '/' + 'protein.mol2')
 
