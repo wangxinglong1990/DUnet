cb943d3fd70bc958764d4874ff203fa4c0f2d89c
wangxinglong1990 <71169505+wangxinglong1990@users.noreply.github.com>
2024-01-10 17:46:17 +0800

    Create dock.sh

---
+++ dock_n_select/dock.sh
@@ -0,0 +1,1 @@
+rosetta_scripts.mpi.linuxgccrelease -s complex.pdb -parser:protocol dock.xml -extra_res_fa LIG.params -nstruct 200 -ex1 -ex2 -use_input_sc -flip_HNQ -no_optH false -overwrite -ignore_unrecognized_res
