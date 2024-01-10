96c6a13298b69dd9bf544b595cf548452840053a
wangxinglong1990 <71169505+wangxinglong1990@users.noreply.github.com>
2024-01-10 17:47:04 +0800

    Update dock.sh

--- dock_n_select/dock.sh
+++ dock_n_select/dock.sh
@@ -1,1 +1,3 @@
+##first thing is to combine the ligand and protein structure file before docking
+cat pro.pdb LIG_0001.pdb > complex.pdb
 rosetta_scripts.mpi.linuxgccrelease -s complex.pdb -parser:protocol dock.xml -extra_res_fa LIG.params -nstruct 200 -ex1 -ex2 -use_input_sc -flip_HNQ -no_optH false -overwrite -ignore_unrecognized_res
